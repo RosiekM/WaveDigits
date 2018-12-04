59f94abf5539b80dadb17c6ba00af6dcf031c5f0
Maciej Palczak <maciejpalczakrzeszow@gmail.com>
2018-12-04 20:39:58 +0100

    delta for mffc
    Added deltas from python speech features to mfcc with parameter N defined in the definition of the function

--- FilesManager.py
+++ FilesManager.py
@@ -2,11 +2,12 @@ from os import *
 import fnmatch
 import scipy.io.wavfile as wav
 from python_speech_features import mfcc
+from python_speech_features import delta
 import numpy as np
 
 
 def importFiles(Path, winlen=0.025, winstep=0.01, numcep=13, nfilt=26, nfft=512, lowfreq=0, highfreq=None, preemph=0.97,
-                ceplifter=22):
+                ceplifter=22, N=None):
     tmp = {}
     digit = []
     type = {}
@@ -23,15 +24,17 @@ def importFiles(Path, winlen=0.025, winstep=0.01, numcep=13, nfilt=26, nfft=512,
                     tmp[x]["name"] = x
                     tmp[x]["lektor"], tmp[x]["typ"], tmp[x]["znak"] = parseName(x)
                     rate, sig = wav.read(Path + "/" + x)
-                    tmp[x]["mfcc"] = mfcc(sig, rate, winlen=winlen, winstep=winstep, numcep=numcep, nfilt=nfilt,
+                    tmp[x]["mfcc"] = mfcc_w_delta=delta(mfcc(sig, rate, winlen=winlen, winstep=winstep, numcep=numcep, nfilt=nfilt,
                                           nfft=nfft, lowfreq=lowfreq, highfreq=highfreq, preemph=preemph,
-                                          ceplifter=ceplifter)
+                                          ceplifter=ceplifter), N)
                     if tmp[x]["znak"] not in digit:
                         digit.append(tmp[x]["znak"])
                     if tmp[x]["typ"] not in type:
                         type[tmp[x]["typ"]] = []
                     if tmp[x]["lektor"] not in type[tmp[x]["typ"]]:
                         type[tmp[x]["typ"]].append(tmp[x]["lektor"])
+
+
 
     except:
         print("permission error in " + Path)
